0da62a76df63e2407f8fd4d85d1076a2fa385922
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-07-05 22:42:11 -0400

    aboutme change

--- portfolio/src/components/Aboutme.jsx
+++ portfolio/src/components/Aboutme.jsx
@@ -2,6 +2,7 @@ const Aboutme = () => {
     return (
         <div className="aboutme">
             <h1>Nice to meet you! <br /> I&apos;m <span>Jonathan Patino.</span></h1>
+            <div className="aboutme_description">Based in Metro Atlanta, </div>
         </div>
     )
 }
